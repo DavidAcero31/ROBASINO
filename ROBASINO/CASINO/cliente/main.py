46ddbe277ebaa9579dcb1fa007ee324f832fd4a0
haroldalfonso <162350982+Harold-Alfonso@users.noreply.github.com>
2026-08-06 02:40:15 -0500

    Implement login screen before main menu
    Added login functionality before main application window.

--- ROBASINO/CASINO/cliente/main.py
+++ ROBASINO/CASINO/cliente/main.py
@@ -1,20 +1,6 @@
-# import tkinter as tk
-# from vistas.pantalla_inicio import PantallaInicio
-
-
-# def main():
-
-#     root = tk.Tk()
-
-#     PantallaInicio(root)
-
-#     root.mainloop()
-
-
-# if __name__ == "__main__":
-#     main()
-
 import tkinter as tk
+
+from vistas.login import Login
 from vistas.menu_principal import MenuPrincipal
 from modelos.jugador import Jugador
 
@@ -22,11 +8,43 @@ from modelos.jugador import Jugador
 def main():
 
     root = tk.Tk()
+
+    # Ocultamos la ventana principal mientras aparece el login
+    root.withdraw()
+
+    login = Login(root)
+
+    # Espera hasta que el usuario cierre el login
+    root.wait_window(login.ventana)
+
+    # Si nunca inició sesión, cerrar la aplicación
+    if not login.autenticado:
+        root.destroy()
+        return
+
+    print("id:", login.id_jugador)
+    print("usuario:", login.usuario)
+    print("nombre:", login.nombre)
+    print("apellido:", login.apellido)
+    print("nivel:", login.nivel)
+    print("pais:", login.pais)
+    print("creditos:", login.creditos)
+
+    # Mostrar la ventana principal
+    root.deiconify()
+
     jugador = Jugador()
-    app = MenuPrincipal(
-        root,
-        jugador,
-        )
+    jugador.client_id = login.client_id 
+
+    jugador.id = login.id_jugador
+    jugador.usuario = login.usuario
+    jugador.nombre = login.nombre
+    jugador.apellido = login.apellido
+    jugador.pais = login.pais
+    jugador.nivel = login.nivel
+    jugador.creditos = login.creditos
+
+    MenuPrincipal(root, jugador, login.conexion)
 
     root.mainloop()
 
